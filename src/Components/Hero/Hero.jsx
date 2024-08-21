357db43015ca9ed509689fe3e03dffcc5f7e80c4
Sheshan Fernando <sheshanatwork@gmail.com>
2024-08-21 20:39:35 +0200

    added lottie and installed the corresponding npm packages as well

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import Lottie from 'lottie-react';
+import AnimationLaptop from '../../assets/laptop-cup.json';
 const Hero = () => {
   return (
     <div>
-      <h1></h1>
+      <h1>hello</h1>
+      <div>
+        <Lottie
+          animationData={AnimationLaptop}
+          loop={true}
+          autoplay={true}
+          style={{ width: 320, height: 320 }}
+        />
+      </div>
     </div>
   );
 };
