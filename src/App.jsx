6563070a4a3041cc2ed0aed059032c8e384167b8
Sheshan Fernando <sheshanatwork@gmail.com>
2024-08-21 20:24:00 +0200

    added components and inside that Hero jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-
+import Hero from './Components/Hero/Hero';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default App;
