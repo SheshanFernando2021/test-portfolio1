1acdcf920ded2dc8bdaf4796aeb6969668a7723c
Sheshan Fernando <sheshanatwork@gmail.com>
2024-08-22 21:45:39 +0200

    added a folder and file within that folder for about me screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Hero from './Components/Hero/Hero';
+import Aboutme from './Components/AboutMe/Aboutme';
 import './App.css';
 
 function App() {
   return (
     <>
       <Hero />
+      <Aboutme />
     </>
   );
 }
---
+++ src/Components/AboutMe/Aboutme.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './aboutme.css';
+
+const Aboutme = () => {
+  return <div className="aboutme-container">this is the about me page</div>;
+};
+
+export default Aboutme;
