7589e3146135dbc98602710095afac194c4d11fc
Sheshan Fernando <sheshanatwork@gmail.com>
2024-08-22 10:50:39 +0200

    added topic and my name and the animation without the background to the hero page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Lottie from 'lottie-react';
 import AnimationLaptop from '../../assets/laptop-cup.json';
+import stack from '../../assets/stack.png';
+import './hero.css';
 const Hero = () => {
   return (
-    <div>
-      <h2>Hello</h2>
-      <div>
+    <div className="hero-container">
+      <div className="hero-right">
+        <h1>Front-End Developer</h1>
+        <p>
+          Hi, I am Sheshan Fernando. I am a 24 year old young Front-end
+          developer
+        </p>
+        <span className="hero-span">
+          <h4>Stack |</h4>
+          <img src={stack} alt="" />
+        </span>
+      </div>
+      <div className="hero-left">
         <Lottie
           animationData={AnimationLaptop}
           loop={true}
           autoplay={true}
-          style={{ width: 320, height: 320 }}
+          style={{ width: 700, height: 700 }}
         />
       </div>
     </div>
