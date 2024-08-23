91858fa684a1e8b85be0a3c80256cad3fd27f89f
Sheshan Fernando <sheshanfernando00@gmail.com>
2024-08-23 22:02:46 +0200

    Last Bit and add the animation

--- src/Components/AboutMe/Aboutme.jsx
+++ src/Components/AboutMe/Aboutme.jsx
@@ -5,24 +5,27 @@ const Aboutme = () => {
   return (
     <div className="aboutme-container">
       <div className="aboutme-leftside">
-        <h1 className="Aboutme">About Me</h1>
-        <div className="description">
-          <h1>description</h1>
-        </div>
-        <div className="buttons">
-          <h1>Buttons</h1>
-        </div>
+        
       </div>
       <div className="aboutme-righttside">
-        <div className="animation">
-          <h1>Animation</h1>
-        </div>
-        <div className="skills">
-          <h1>Skills</h1>
-        </div>
+
+      <h1 className="Aboutme">About Me</h1>
+        <div className="description">
+          <p>Hi, I'm Sheshan Fernando, a passionate front-end developer with over two years of professional experience in creating dynamic and visually engaging web applications. Originally from Sri Lanka and currently based in Italy, I am 24 years old and pursuing a Bachelor's degree in Artificial Intelligence at the University of Pavia Italy.
+
+            I specialize in a modern tech stack that includes JavaScript, React.js, WordPress, Firebase, HTML, CSS, Git, GitHub, and Tailwind CSS. My journey as a freelancer has allowed me to hone my skills and deliver high-quality solutions to diverse clients. However, my enthusiasm for front-end development drives me to seek a permanent role where I can contribute my expertise and continue to grow in a collaborative environment.
+
+            I am dedicated to crafting user-centric experiences and leveraging my technical skills to build responsive and interactive web applications. Let's connect and explore how I can bring value to your team!</p>        </div>
+        {/* <div className="buttons-container">
+          <h3>Downloads</h3>
+          <div className="buttons"><button>Cover Letter</button>
+          <button>CV</button></div>
+          
+        </div> */}
       </div>
     </div>
   );
 };
 
 export default Aboutme;
+                
