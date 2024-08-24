f7956c5abb62dc0f302f5f05093300448847eabd
Sheshan Fernando <sheshanfernando00@gmail.com>
2024-08-24 13:24:52 +0200

    renamed allthe old ul to desktopVersion and new ul to mobileVersion and adjusted the displaying widths

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './navbar.css';
+import menu from '../../assets/dropdownmenu.svg';
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
+      <ul className='desktopVersion'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#aboutme">About Me</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact me</a></li>
+        <li><a href="#arcade">Arcade</a></li>
+      </ul>
+      <div className='mobileVersion'>
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#aboutme">About Me</a></li>
@@ -11,6 +20,7 @@ const Navbar = () => {
         <li><a href="#contact">Contact me</a></li>
         <li><a href="#arcade">Arcade</a></li>
       </ul>
+      </div>
     </div>
   );
 };
